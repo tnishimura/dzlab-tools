2fa499a1c9029978cdc09e4913063d6881d98d76
Toshiro Nishimura <toshiro@oberon.dzlab.pmb.berkeley.edu>
2011-05-18 21:28:45 -0700

    index_fastq.pl -> fastq_build_index.pl. store sequence as well, instead of just byte coords

---
+++ fastq_build_index.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+use feature 'say';
+use autodie;
+use DBI;
+use Log::Log4perl qw/:easy/;
+use File::Temp qw/mktemp/;
+
+Log::Log4perl->easy_init( { 
+    level    => $DEBUG,
+    layout   => '%d{HH:mm:ss} %p> (%L) %M - %m%n',
+} );
+my $logger = get_logger();
+
+die "usage: $0 sequence.fastq" unless @ARGV==1;
+open my $fh, '<', $ARGV[0];
+
+my $outfile = $ARGV[0] . ".index";
+unlink $outfile if -f $outfile;
+
+my $tmpfile = mktemp($outfile . ".tmp.XXXXX");
+
+#######################################################################
+# Get file size
+
+my $size = (stat($ARGV[0]))[7];
+
+#######################################################################
+# Config db
+
+my $sth;
+my $dbh = DBI->connect("dbi:SQLite:dbname=$tmpfile","","", {RaiseError => 1, AutoCommit => 1});
+$dbh->do("PRAGMA automatic_index = OFF");
+$dbh->do("PRAGMA journal_mode = OFF");
+$dbh->do("PRAGMA cache_size = 80000");
+$dbh->do("create table fastq (id,sequence)");
+$sth = $dbh->prepare("insert into fastq (id,sequence) values (?,?)");
+$dbh->{AutoCommit} = 0;
+
+#######################################################################
+# Loop
+
+my $progress_step       = .05;
+my $progress_checkpoint = .05;
+my $commit_size         = 20000;
+my $counter             = 0;
+my $position            = tell $fh;
+
+while (defined(my $line = <$fh>)){
+    chomp $line;
+    $logger->logdie("malformatted line $line") unless $line =~ /^@/;
+
+    $line =~ s/^@//;
+
+    $sth->execute($line,$position);
+    if (++$counter % $commit_size == 0){
+        #$logger->debug("$counter");
+        $dbh->commit or $logger->logdie("Couldn't commit?");
+    }
+    if ($position / $size > $progress_checkpoint){
+        $logger->debug(sprintf("%.2f%%" , 100 * $position / $size));
+        $progress_checkpoint += $progress_step;
+    }
+    for (1..3){
+        if (! defined <$fh>){
+            $logger->logdie("number of lines not a multiple of 4");
+        }
+    }
+    $position = tell $fh;
+}
+$logger->debug("Done inserting. Creating index");
+
+$dbh->commit;
+$dbh->{AutoCommit} = 1;
+$dbh->do("create index idx1 on fastq (id)");
+
+$logger->debug("Done creating index");
+
+$dbh->disconnect or $logger->logdie("Couldn't disconnect?");
+
+rename $tmpfile, $outfile or $logger->logdie("Couldn't create $outfile");
