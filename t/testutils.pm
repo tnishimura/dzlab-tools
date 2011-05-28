5a4f53ea11a664543c61dda70cee239524a58f44
Pareto <Pareto@example.org>
2011-05-28 02:14:16 -0700

    clean testutils.pm

--- t/testutils.pm
+++ t/testutils.pm
@@ -12,7 +12,7 @@ use List::Util qw/min max/;
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw();
-our @EXPORT = qw(overlap sort_results gen_rand_tree search_linear randrange);
+our @EXPORT = qw(sort_results gen_rand_tree search_linear randrange);
 
 my $max = 100000;
 my $N   =   1000;
@@ -40,23 +40,6 @@ sub gen_rand_tree{
 
 sub sort_results{
     return [sort {$a->{item} cmp $b->{item}} @_];
-}
-
-# utility. return number of units overlapped but $x, $y. 
-sub overlap{
-    my ($x,$y) = @_;
-    my $start1 = min($x->[0], $x->[1]);
-    my $end1   = max($x->[0], $x->[1]);
-
-    my $start2 = min($y->[0], $y->[1]);
-    my $end2   = max($y->[0], $y->[1]);
-
-    if ($end1 >= $start2 && $end2 >= $start1){
-        return min($end1, $end2) - max($start1, $start2)  + 1;
-    }
-    else {
-        return 0;
-    }
 }
 
 1;
