ea8ad0f914d025a884b51cc9dfa354c94a4d9aca
Toshiro Nishimura <toshiro@oberon.dzlab.pmb.berkeley.edu>
2011-06-10 16:25:56 -0700

    makedocs - working

--- util/makedocs.pl
+++ util/makedocs.pl
@@ -8,13 +8,15 @@ use Getopt::Euclid qw( :vars<opt_> );
 use Pod::Usage;
 use File::Basename;
 use File::Spec::Functions;
-pod2usage(-verbose => 99,-sections => [qw/NAME SYNOPSIS OPTIONS/]) if $opt_help;
+pod2usage(-verbose => 99,-sections => [qw/NAME SYNOPSIS OPTIONS/]) if !$opt_outdir;
+
+
 
 if (! -d $opt_outdir){
     mkdir $opt_outdir;
 }
 
-for my $script (glob("*.pl *.pod")) {
+for my $script (glob("*.pl doc/*.pod")) {
     my $out = catfile($opt_outdir, basename($script)) . ".html";
     system("podtree2html $script $out");
 }
