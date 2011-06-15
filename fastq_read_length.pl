3480bc5086c03d9cbdce6253ff33242c650cd1ff
Toshiro Nishimura <toshiro@miranda.dzlab.pmb.berkeley.edu>
2011-06-15 02:33:24 -0700

    fastq_read_length.pl takes multiple arguments

--- fastq_read_length.pl
+++ fastq_read_length.pl
@@ -9,14 +9,18 @@ use FindBin;
 use lib "$FindBin::Bin/lib";
 use DZUtil qw/fastq_read_length/;
 
-if (@ARGV != 1){
-    say "usage: $0 alignment.fastq";
+if (@ARGV == 0){
+    say "usage: $0 alignment.fastq .. ";
     exit 1;
 }
 
-my $length = fastq_read_length($ARGV[0]);
+for my $file (@ARGV) {
+    # body...
+    my $length = fastq_read_length($file);
 
-if (! defined $length){
-    say "$0: fastq file malformed?";
+    if (! defined $length){
+        say "$0: fastq file $file malformed?";
+    }
+    say "$file: $length";
 }
-say $length;
+
