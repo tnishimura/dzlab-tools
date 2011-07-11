7dc777197acef35e8283c020a8e72dd026e7f06a
Toshiro Nishimura <toshiro@oberon.dzlab.pmb.berkeley.edu>
2011-07-11 15:25:44 -0700

    single_c_stats.pl - don't use gff::parser, too slow

--- single_c_stats.pl
+++ single_c_stats.pl
@@ -8,7 +8,6 @@ use autodie;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use GFF::Parser;
-use List::Util qw/sum/;
 
 END {close STDOUT}
 $|=1;
@@ -16,23 +15,40 @@ $|=1;
 
 say "file\tcount\tmean\tmedian";
 
+sub sum{
+    my $arr = shift;
+    my $total = 0;
+    my $count = 0;
+    for (@$arr) {
+        if (defined $_){
+            $total += $_;
+            $count += 1;
+        }
+    }
+    return ($total, $count);
+}
+
 
 for my $file (@ARGV) {
+    my $filesize = (stat $file)[7];
+    my $index = 0;
     my $p = GFF::Parser->new(file => $file);
+    open my $fh, '<', $file;
     my @cplust;
-    while (defined(my $gff = $p->next())){
-        my ($c, $t) = ($gff->get_column('c'), $gff->get_column('t'));
-        next unless defined $c && defined $t;
-        push @cplust, $c+$t;
+    $#cplust = int $filesize/50;
+    #while (defined(my $gff = $p->next())){
+    while (defined(my $line = <$fh>)){
+        chomp $line;
+        if ($line =~ /c=(\d+);t=(\d+)/){
+            $cplust[$index++] = $1+$2;
+        }
     }
     @cplust = sort { $a <=> $b } @cplust;
-    my $length = scalar @cplust;
-    my $mean   = $length ? sum(@cplust) / $length : 0;
+    #say Dumper \@cplust;
+    my ($sum, $length) = sum(\@cplust);
+    my $mean   = $length ? $sum / $length : 0;
     my $median = $length ? $cplust[int($length/2)]: 0;
     printf "%s\t%d\t%.4f\t%.4f\n", $file, $length, $mean, $median;
     @cplust = ();
 }
-
-
-
 
