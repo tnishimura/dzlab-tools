0b68cdce93d47f1d126637479068a6f62653e9ba
Toshiro Nishimura <toshiro@oberon.dzlab.pmb.berkeley.edu>
2011-07-21 19:15:21 -0700

    single_c_concat.pl

---
+++ single_c_concat.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use lib "/wip/tools/dzlab-tools/lib";
+use autodie;
+use Config::General qw(ParseConfig);
+use Data::Dumper;
+use feature 'say';
+use File::Basename;
+use File::Find;
+use File::Spec::Functions;
+use DZUtil qw/localize common_prefix common_suffix timestamp/;
+
+die "usage: $0 contextual-single-c-files.gff ..." unless @ARGV;
+
+my $prefix = common_prefix(@ARGV);
+my $suffix = common_suffix(@ARGV);
+$prefix =~ s/\.$|\_$//;
+$suffix =~ s/^\.|\^_//;
+my $concat  = $prefix . ".all." . $suffix;
+
+open my $outfh, '>', $concat;
+for my $f (@ARGV) {
+    open my $infh, '<', $f;
+    while (defined(my $line = <$infh>)){
+        chomp $line;
+        say $outfh $line;
+    }
+    close $infh;
+}
+close $outfh;
