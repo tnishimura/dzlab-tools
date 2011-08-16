1fefcdb1aa9628884a37122705b07729f2bd0ea4
Toshiro Nishimura <toshiro@oberon.dzlab.pmb.berkeley.edu>
2011-08-15 17:07:35 -0700

    gff_subtract.pl - uneven inputs

--- gff_subtract.pl
+++ gff_subtract.pl
@@ -4,28 +4,55 @@ use warnings FATAL => "all";
 use 5.010_000;
 use Data::Dumper;
 use autodie;
+use FindBin;
+use lib "$FindBin::Bin/lib";
+use GFF::Parser;
+use GFF;
 
 END {close STDOUT}
 $| = 1;
 
 if (2 != @ARGV){
-    say "$0 - subtract X's scores from Y's and output new GFF";
-    say "usage: $0 X.gff Y.gff > result.gff";
+    say <<"END";
+$0 - subtract X's scores from Y's and output new GFF
+usage: $0 X.gff Y.gff > result.gff
+
+NOTE: X.gff and Y.gff are assumed to be sorted with a command like
+ sort -f -k1,1 -k4,4n X.gff -o X.gff
+END
     exit 1;
 }
 
 my @files = @ARGV;
 
-open my $left_fh, '<', $files[0];
-open my $right_fh, '<', $files[1];
 
-while (
-    defined (my $left_line = <$left_fh>) and 
-    defined (my $right_line = <$right_fh>) 
-){
-    chomp($left_line, $right_line);
-    my @left = split /\t/, $left_line;
-    my @right = split /\t/, $right_line;
 
-    say join "\t", @left[0 .. 4], (($left[5] // 0) - ($right[5] //0)), @right[6..8];
+my $parser_left = GFF::Parser->new(file => $files[0], normalize => 0);
+my $parser_right = GFF::Parser->new(file => $files[1], normalize => 0);
+
+LEFT:
+while (defined(my $left = $parser_left->next)){
+    RIGHT:
+    while (defined(my $right = $parser_right->next)){
+        if(GFF::start_position_equal($left,$right)){
+            say join "\t", $left->sequence, $left->source // '.', $left->feature // '.', 
+            $left->start, $left->end, ($left->score - $right->score), $left->strand // '.', $left->frame //'.',
+            $left->attribute_string // '.';
+            last RIGHT;
+        }
+        elsif(GFF::start_position_lessthan($left,$right)){
+            $parser_right->rewind($right);
+            say $left->to_string;
+            last RIGHT;
+        }
+        elsif(GFF::start_position_lessthan($right,$left)){
+            say join "\t", $right->sequence, $right->source // '.', $right->feature // '.', 
+            $right->start, $right->end, (0 - $right->score), $right->strand // '.', $right->frame //'.',
+            $right->attribute_string;
+        }
+    }
+}
+while (defined(my $right = $parser_right->next)){
+    $right->score(-$right->score);
+    say $right->to_string;
 }
