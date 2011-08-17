5ac9edbd750d6ea4aff82ded1c85ce925b99d133
Toshiro Nishimura <toshiro@oberon.dzlab.pmb.berkeley.edu>
2011-08-17 15:06:03 -0700

    single_c_raw_context.pl - got rid of GFF::Parser dependency for speed

--- single_c_raw_context.pl
+++ single_c_raw_context.pl
@@ -6,7 +6,6 @@ use Data::Dumper;
 use autodie;
 use FindBin;
 use lib "$FindBin::Bin/lib";
-use GFF::Parser;
 use FastaReader;
 use Getopt::Euclid qw( :vars<opt_> );
 use Pod::Usage;
@@ -21,26 +20,35 @@ if $opt_help || ! defined $opt_reference || ! @opt_inputs;
 my $fasta = FastaReader->new(file => $opt_reference, slurp => 1);
 
 for my $file (@opt_inputs) {
-    my $parser = GFF::Parser->new(file => $file, normalize => 0);
+    #my $parser = GFF::Parser->new(file => $file, normalize => 0);
+    open my $gff_fh, '<', $file;
 
     (my $filename_template = $file) =~ s/\.gff$//;
     my %filehandles;
     my %counts;
 
-    while (defined(my $gff = $parser->next())){
-        my $forward = $fasta->get_context_raw($gff->sequence, $gff->start, rc => 0);
-        my $reverse = $fasta->get_context_raw($gff->sequence, $gff->start, rc => 1);
+
+    while (defined(my $gff = <$gff_fh>)){
+        chomp $gff;
+        my @gff_split = split /\t/, $gff;
+        next unless @gff_split == 9;
+
+        my $forward = $fasta->get_context_raw($gff_split[0], $gff_split[3], rc => 0);
+        my $reverse = $fasta->get_context_raw($gff_split[0], $gff_split[3], rc => 1);
         my $raw = ($forward =~ /^C/) ? $forward : $reverse;
+
+        next if $raw !~ /[ACGT]{3}/;
 
         next unless (length $raw == 3);
         if (! exists $filehandles{$raw}){
             open $filehandles{$raw}, '>', "$filename_template-$raw.gff";
         }
-        $gff->feature($gff->feature() . "-$raw");
-        say {$filehandles{$raw}} $gff->to_string();
+        $gff_split[2] .= "-$raw";
+        say {$filehandles{$raw}} join "\t", @gff_split;
         ++$counts{$raw};
     }
     close $_ for values %filehandles;
+    close $gff_fh;
     # log
     {
         open my $log, '>', "$filename_template-rawcont_log.txt";
@@ -62,13 +70,15 @@ single_c_raw_context.pl - separate single-c file by raw triplet context.
 
 Usage examples:
 
- single_c_raw_context.pl -r genome.fasta 
+ single_c_raw_context.pl -r genome.fasta input.gff
 
-=head1 REQUIRED ARGUMENTS
+If, for example, input.gff is a CG file, this command would produce the following files:
 
-=over
-
-=back
+ input-CGA.gff
+ input-CGC.gff
+ input-CGG.gff
+ input-CGT.gff
+ input-rawcont_log.txt # contains counts and proportion of every context
 
 =head1 OPTIONS
 
