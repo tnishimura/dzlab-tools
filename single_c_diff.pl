c4e8d148b3072613d8c9a2f710acd0f9df8df692
Toshiro Nishimura <toshiro@oberon.dzlab.pmb.berkeley.edu>
2011-08-24 16:24:06 -0700

    single_c_diff.pl - print yaml of stats

--- single_c_diff.pl
+++ single_c_diff.pl
@@ -7,26 +7,61 @@ use autodie;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use GFF::Parser;
+use YAML qw/Dump/;
 
 END {close STDOUT}
 $| = 1;
 
-if (@ARGV != 2){
+if (@ARGV < 2){
     say "usage: $0 superset supposed-subset";
     exit 1;
 }
 
-my $superset_parser = GFF::Parser->new(file => $ARGV[0]);
-my $subset_parser = GFF::Parser->new(file => $ARGV[1]);
+my $superset_parser = GFF::Parser->new(file => shift);
 
 my %superset_positions;
+my %stats = (
+    found_and_underscore => 0,
+    correct => 0,
+    overscore => 0,
+    error => 0,
+    total_real_c => 0,
+    total_detected_c => 0,
+    non_methylated_detected => 0,
+);
 
 while (defined(my $gff = $superset_parser->next())){
     $superset_positions{$gff->sequence,$gff->start} = $gff->score;
+    $stats{total_real_c}+=$gff->score;
 }
 
-while (defined(my $gff = $subset_parser->next())){
-    if ($gff->score > 0 && ! exists $superset_positions{$gff->sequence,$gff->start}){
-        say $gff->to_string();
+
+for my $singlec (@ARGV) {
+    say STDERR "processing $singlec";
+    my $subset_parser = GFF::Parser->new(file => $singlec);
+    while (defined(my $gff = $subset_parser->next())){
+        my $subset_count = $gff->score > 0 ? $gff->get_column('c') : 0;
+        my $superset_count = 
+        exists $superset_positions{$gff->sequence,$gff->start} ?  $superset_positions{$gff->sequence,$gff->start} : 0;
+
+        if ($subset_count > 0 && $superset_count ==0){
+            $stats{error}++;
+        }
+        elsif ($subset_count > 0 && $superset_count > $subset_count){
+            $stats{found_and_underscore}++;
+        }
+        elsif ($subset_count > 0 && $superset_count == $subset_count){
+            $stats{correct}++;
+        }
+        elsif ($subset_count > 0 && $superset_count < $subset_count){
+            $stats{overscore}++;
+        }
+        elsif ($subset_count == 0){
+            $stats{non_methylated_detected}++;
+        }
+        $stats{total_detected_c}+=$subset_count;
     }
+
 }
+
+say Dump \%stats;
