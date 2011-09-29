5284e68265acb3c33aefdc90cc799b877b940c25
tnish <tnish@berkeley.edu>
2011-09-28 23:50:35 -0700

    arma.pl - alias for arma.pl

---
+++ arma.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use strict;
+use warnings FATAL => "all";
+use 5.010_000;
+use Data::Dumper;
+use autodie;
+
+END {close STDOUT}
+$| = 1;
+
+system(qw/perl -S armageddon_analysis.pl/, @ARGV);
