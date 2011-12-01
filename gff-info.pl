6dbb7a6dfeeb63875f016190b9dcce03749f9b8b
tnish <tnish@berkeley.edu>
2011-12-01 15:20:39 -0800

    gff-info.pl - spit out yaml

--- gff-info.pl
+++ gff-info.pl
@@ -13,33 +13,39 @@ use Statistics::Descriptive;
 END {close STDOUT}
 $| = 1;
 
+use YAML qw/Bless Dump/;
+
+my %yaml;
 
 for my $file (@ARGV) {
     my $count = 0;
-    my %seqs;
-    my %features;
-
     my $lengths = Statistics::Descriptive::Full->new();
+    my %file_yaml;
 
     my $parser = GFF::Parser->new(file => $file, normalize => 0);
     while (defined(my $gff = $parser->next())){
         my ($seq, $feature) = ($gff->sequence(), $gff->feature());
-        $seqs{$seq}++;
-        $features{$feature}++;
-        $count++;
+
+        $file_yaml{sequences}{$seq}++;
+        $file_yaml{features}{$feature}++;
+        $file_yaml{count}++;
+
         $lengths->add_data($gff->end - $gff->start + 1);
     }
-    say "### $file";
-    printf("# Length mean std (min, 25%%, 50%%, 75%%, max): %.1f %.1f (%d,%d)\n", 
-        $lengths->mean(), 
-        $lengths->standard_deviation(),
-        $lengths->min(), 
-        $lengths->max(), 
-    );
-    printf("# Sequences (%d total):\n", scalar(keys %seqs));
-    for my $seq (sort keys %seqs) { say "$seq\t$seqs{$seq}"; }
 
-    printf("# Features (%d total):\n", scalar(keys %features));
-    for my $fet (sort keys %features) { say "$fet\t$features{$fet}"; }
+    $file_yaml{mean}                = sprintf "%.1f", $lengths->mean();
+    $file_yaml{standard_deviation}  = sprintf "%.1f", $lengths->standard_deviation();
+    $file_yaml{min}                 = $lengths->min();
+    $file_yaml{median}              = $lengths->median();
+    $file_yaml{max}                 = $lengths->max();
+    $file_yaml{number_of_sequences} = scalar(keys %{$file_yaml{sequences}});
+    $file_yaml{number_of_features}  = scalar(keys %{$file_yaml{features}});
+
+    Bless(\%file_yaml)->keys([qw/
+        count mean standard_deviation min median max number_of_sequences sequences number_of_features features
+        /]);
+
+    $yaml{$file} = \%file_yaml;
 }
 
+say Dump \%yaml;
