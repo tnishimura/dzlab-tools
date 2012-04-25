b8b6ec452dbfe8aebb9009834057bac0e1512b0d
tnish <tnish@berkeley.edu>
2012-04-25 16:32:23 -0700

    red-headed-step-child.pl - working

--- red-headed-step-child.pl
+++ red-headed-step-child.pl
@@ -11,102 +11,105 @@ use Getopt::Long;
 use Parallel::ForkManager;
 use List::Util qw/sum/;
 use List::MoreUtils qw/ any notall/;
-
 use YAML qw/LoadFile DumpFile/;
 
 use FindBin;
 use lib "$ENV{HOME}/dzlab-tools/lib";
 use FastqReader;
 use FastaReader;
-use DZUtil qw/c2t md5sum/;
+use DZUtil qw/c2t/;
+use Digest::MD5::Util;
 use Launch qw/cast/;
+use Run::BowtieBuild;
 
 END {close STDOUT}
 $| = 1;
+
+my @flank_range;
 
 my $result = GetOptions (
     #"tdna-file|tf=s"       => \(my $tdna_file),
     #"reads-file|rf=s"      => \(my $reads_file),
     #"flank-seq-file|ff=s"  => \(my $flank_seq_file),
-    "outdir|o=s"           => \(my $outdir),
-    "flank-size|fs=i"      => \(my $flank_size = 30),
-    "flank-mismatch|fm=i"  => \(my $flank_mismatch = 2),
-    "tdna-mismatch|tm=i"   => \(my $tdna_mismatch = 2),
-    "gap|g=i"              => \(my $gap = 5),
-    "prefix-min-size|pm=i" => \(my $prefix_min_size = 20),
-    "parallel|p=i"         => \(my $parallel = 0),
+    "outdir|o=s"             => \(my $outdir),               # should already exist
+    "reference|r=s"          => \(my $reference),            # reference file
+
+    "flank-range|fr=i{2}"    => \@flank_range,               # this range of flank becomes range
+    "flank-mismatch|fm=i"    => \(my $flank_mismatch = 3),   # align bait to reads with this many mm
+
+    "prefix-trim5|p5=i"      => \(my $prefix_trim5 = 0),     # trim this much from prefix 5'
+    "prefix-trim3|p3=i"      => \(my $prefix_trim3 = 5),     # trim this much from prefix 3'
+    "prefix-min-size|pmin=i" => \(my $prefix_min_size = 20), # if trimmed is smaller, discard
+    "prefix-max-size|pmax=i" => \(my $prefix_max_size = 30), # if trimmed is bigger, trim 3' further
+    "prefix-mismatch|tm=i"   => \(my $prefix_mismatch = 3),  # align prefixes to tdna/ref with this many mm
+
+    "parallel|p=i"           => \(my $parallel = 0),
 );
 
-usage() if (!$result);
+@flank_range = @flank_range ? @flank_range : (1, 30);
+
+usage("malformed arguments") if (!$result);
+usage() if ! @ARGV;
 
 my ($tdna_file, $flank_seq_file, $reads_file) = @ARGV;
-
-usage() if any { ! defined $_ || ! -f $_ } ($tdna_file, $flank_seq_file, $reads_file);
-usage() if (! defined $outdir || ! -d $outdir);
+usage("tdna, flank, reads file all need to exist") if any { ! defined $_ || ! -f $_ } ($tdna_file, $flank_seq_file, $reads_file);
+usage("no reference given") if (!$reference);
+usage("output directory dne") if (! defined $outdir || ! -d $outdir);
 
 my $pm = Parallel::ForkManager->new($parallel);
 
 sub usage {
     $0 = basename($0);
-    say "$0 [options] -o outdir tdna-file flank-seq-file reads-file";
+    say $_[0] if @_;
+    say "usage: $0 [options] -o outdir tdna-file flank-seq-file reads-file";
     exit 1;
 }
 
 #######################################################################
 
-my $bait_file = catfile($outdir, "chromosomal-bait");
+my $bait_file = catfile($outdir, "bait_$flank_range[0]_$flank_range[1]");
 my $split_dir = catdir($outdir, "split_reads");
 make_path $split_dir;
 my $split_prefix = catdir($split_dir, basename($reads_file)) . ".";
-my $split_hash_file = $split_prefix . "MD5SUM";
-my $prefix_file = catfile($outdir, basename($reads_file) . ".prefix.fasta");
-my $tdna_file_bsrc = catfile($outdir, basename($tdna_file) . ".bsrc");
+my $split_checksum_file = $split_prefix . "MD5SUM";
+
+
+my $tdna_file_bsrc; 
+my $reference_bsrc;
 
 #######################################################################
-# create bait from flank seq 
+# create bait file from flank seq 
 {
     my $fr = FastaReader->new(file => $flank_seq_file);
     my @seqs_in_flank = $fr->sequence_list();
     if (! @seqs_in_flank) {
         die "flank file empty";
     }
-    if (@seqs_in_flank != 1) {
-        die "why are the more than one seqs in flank file?";
-    }
-    my $bait = c2t substr $fr->get($seqs_in_flank[0]), 0, $flank_size;
     open my $fh, '>', $bait_file;
-    say $fh ">chrbait";
-    say $fh $bait;
+    for my $flankseq (@seqs_in_flank) {
+        my $bait = $fr->get($flankseq, @flank_range, bs => 'c2t', lenient => 1);
+        say $fh ">${flankseq}_$flank_range[0]_$flank_range[1]";
+        say $fh $bait;
+    }
     close $fh;
 }
 
 #######################################################################
 # create bsrc tdna
 
-{
-    FastaReader::bsrc_file($tdna_file, $tdna_file_bsrc, 'c2t');
-
-    cast "bowtie-build --noref $tdna_file_bsrc $tdna_file_bsrc";
-}
+($tdna_file_bsrc) = bowtie_build(file => $tdna_file, rc => 1, bs => 'c2t');
+($reference_bsrc) = bowtie_build(file => $reference, rc => 1, bs => 'c2t');
 
 #######################################################################
-# split the reads in smaller chunks (and convert to fasta/c2t)
+# split the reads in smaller chunks (while converting to fasta/c2t)
 
-my @split_reads;
+my @split_read_files;
 SPLIT:
 {
-    if (-f $split_hash_file && defined(my $split_hashes = LoadFile($split_hash_file))){
-        my $ok = 1;
-        while (my ($file,$hash) = each %$split_hashes) {
-            if (md5sum($file) ne $hash){
-                $ok = 0;
-                last;
-            }
-        }
-        if ($ok){
-            @split_reads = keys %$split_hashes;
-            last SPLIT;
-        }
+    if (-f $split_checksum_file && defined(my $confirmed_files =md5_confirm($split_checksum_file))){
+        @split_read_files = @$confirmed_files;
+        say "splitting of reads was already done";
+        last SPLIT;
     }
 
     my $max_per_file = 943_718_400; # 900MB
@@ -115,7 +118,7 @@ SPLIT:
 
     my $fqr = FastqReader->new(file => $reads_file, linesper => 4);
     open my $writer, '>', $split_prefix . $suffix;
-    push @split_reads, $split_prefix . $suffix;
+    push @split_read_files, $split_prefix . $suffix;
     while (defined(my $fq = $fqr->next())){
         my ($readid, $sequence) = @$fq;
         $sequence = c2t $sequence;
@@ -129,53 +132,46 @@ SPLIT:
             close $writer;
             $suffix++;
             open $writer, '>', $split_prefix . $suffix;
-            push @split_reads, $split_prefix . $suffix;
+            push @split_read_files, $split_prefix . $suffix;
             $bytes_written = 0;
         }
     }
     close $writer;
     
-    @split_reads = map { rel2abs($_) } @split_reads;
+    @split_read_files = map { rel2abs($_) } @split_read_files;
 
-    DumpFile($split_hash_file, {
-            map {$_ => md5sum($_)} @split_reads
-        });
+    md5_create_checksum($split_checksum_file, @split_read_files);
 }
 
-#######################################################################
-# bowtie-build
-
-for my $split (@split_reads) {
+for my $split (@split_read_files) {
     $pm->start and next;
-    if (notall { -f && -s } map { "$split.$_" } qw/1.ebwt 2.ebwt rev.1.ebwt rev.2.ebwt/){
-        cast "bowtie-build --noref $split $split";
-    }
-    else{
-        say "bowtie-build on $split already done";
-    }
+    bowtie_build(file => $split, noref => 1);
     $pm->finish; 
 }
 $pm->wait_all_children;
 
 #######################################################################
 
-my %reads; # split_read_file => { read_id => position }
+my %aligned_reads; # split_read_file => { read_id => position }
 
 # run each bowtie in separate process, collect reads and start positions
-$pm->run_on_finish(sub{ # call before calling start()
+$pm->run_on_finish(sub{ 
         my $ref = $_[5];
         if (defined($ref)) {  
-            my ($split_read, $id_to_startpos) = @$ref;
-            $reads{$split_read} = $id_to_startpos;
+            my ($split_read_file, $id_to_startpos_href) = @$ref;
+            $aligned_reads{$split_read_file} = $id_to_startpos_href;
         } 
     });
 
-for my $split_read (@split_reads){
-    my $bowtie_output = catfile($outdir, basename($bait_file) . "-vs-" . basename($split_read) . ".mm$flank_mismatch");
+for my $split_read (@split_read_files){
+    my $bowtie_output = catfile($outdir, basename($bait_file) . "-vs-" . basename($split_read) . ".mm${flank_mismatch}.bowtie");
 
     $pm->start and next;
     if (! -f $bowtie_output){
         cast "bowtie --norc -f -v $flank_mismatch $split_read $bait_file > $bowtie_output",
+    }
+    else{
+        say STDERR "bowtie already done ($bowtie_output)";
     }
 
     my $return_value = [ $split_read, {} ];
@@ -195,20 +191,39 @@ $pm->wait_all_children;
 
 #######################################################################
 
+my $prefix_file = 
+catfile($outdir, basename($bait_file)) . 
+"-vs-" . 
+basename($reads_file) . 
+".mm${flank_mismatch}.prefix-trim$prefix_trim5-$prefix_trim3.size$prefix_min_size-$prefix_max_size.fasta";
+
 if (! -f $prefix_file){
     say STDERR "grabbing prefixes from bowtie results";
 
     open my $prefix_out, '>', $prefix_file;
-    while (my ($split_reads_file,$id_to_startpos) = each %reads) {
+    while (my ($split_reads_file,$id_to_startpos) = each %aligned_reads) {
         my $fqr = FastqReader->new(file => $split_reads_file, linesper => 2);
         my $id2seq = $fqr->get_reads(keys %$id_to_startpos);
         next if (keys %$id_to_startpos == 0);
 
         while (my ($id,$seq) = each %$id2seq) {
-            my $position = $id_to_startpos->{$id};
-            if ($position - 5  > 20){
-                say $prefix_out ">PREFIX_$id";
-                say $prefix_out substr $seq, 0, $position;
+            my $whole_prefix_size = $id_to_startpos->{$id}; # position reported in bait file bowtie is size of prefix
+            my $length = $whole_prefix_size - $prefix_trim3 - $prefix_trim5;
+
+            if ($length >= $prefix_min_size){
+                my $offset = $prefix_trim5;
+                
+                if ($length > $prefix_max_size){
+                    $length = $prefix_max_size;
+                }
+
+                {
+                    my $start = $offset + 1;
+                    my $end = $start + $length - 1;
+                    say $prefix_out ">PREFIX_${id}_${start}_${end}";
+                }
+
+                say $prefix_out substr $seq, $offset, $length;;
             }
         }
     }
@@ -218,4 +233,16 @@ else{
     say STDERR "prefix file already done";
 }
 
-cast("bowtie --norc -f -B 1 -v $tdna_mismatch $tdna_file_bsrc $prefix_file");
+my $prefix_vs_tdna = "$prefix_file-vs-tdna.mm$prefix_mismatch";
+my $prefix_vs_reference = "$prefix_file-vs-reference.mm$prefix_mismatch";
+
+cast("bowtie --norc -f -B 1 -v $prefix_mismatch $tdna_file_bsrc $prefix_file > $prefix_vs_tdna");
+cast("bowtie --norc -f -B 1 -v $prefix_mismatch $reference_bsrc $prefix_file > $prefix_vs_reference");
+
+=head2 INTERNALS
+
+=head3 Step one - create bait from flank
+
+=head3
+
+=cut
