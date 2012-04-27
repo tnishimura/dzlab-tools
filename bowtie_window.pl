ac35db000af9e8f37edbcfa44e0da6cf861ee7c5
tnish <tnish@berkeley.edu>
2012-04-26 18:45:00 -0700

    bowtie_window.pl - uses BigArray

--- bowtie_window.pl
+++ bowtie_window.pl
@@ -4,35 +4,89 @@ use warnings FATAL => "all";
 use 5.010_000;
 use Data::Dumper;
 use autodie;
-END {close STDOUT}
-$| = 1;
-use FindBin;
-use lib "$FindBin::Bin/lib";
-use Launch;
-
 use Pod::Usage;
 use Getopt::Long;
+use FindBin;
+use lib "$FindBin::Bin/lib";
+use BowtieParser;
+use BigArray;
+use FastaReader;
+END {close STDOUT}
+$| = 1;
 
 my $result = GetOptions (
-    #"longname|l=isf" => \my $var,
-    "reference|r=s" => \my $reference,
-    "bowtie|b=s" => \my $bowtie,
-    "window-by-fixed|w=i" => \(my $window_size = 50),
+    "reference|r=s"       => \my $reference,
+    "window-size|w=i" => \(my $window_size = 50),
+    "no-skip|k"           => \(my $noskip),
+    "base|b=i"            => \(my $base = 1),
 );
-if (!$result || !$bowtie || !$reference){
-    say "usage: bowtie_window.pl -b bowtie -r genome";
+
+if (!$result || !$reference){
+    say "usage: bowtie_window.pl -r genome --no-skip --base 1 --window-size 50 bowtiefile";
     exit 1;
 }
 
-my $basename = $bowtie;
-$basename =~ s/\.bowtie$//;
+my $bowtie_reader = BowtieParser->new(file => \*ARGV);
+my $fasta_reader = FastaReader->new(file => $reference, slurp => 0);
 
-my $gff = "$basename.gff";
-my $w50 = "$basename.w$window_size.gff";
+my %counters = map { 
+    uc($_) => BigArray->new(base => $base, size => $fasta_reader->get_length($_))
+} $fasta_reader->sequence_list();
 
-launch("perl -S parse_bowtie.pl -g --windowable -o ?? $bowtie",
-    expected => $gff,
-);
-launch("perl -S window_by_fixed.pl --count-in-scores -n -w $window_size -k -r $reference -o ?? $gff",
-    expected => $w50,
-);
+my $counter = 0;
+while (defined(my $bowtie = $bowtie_reader->next())){
+    my (undef, undef, $chr, $pos, $read) = @$bowtie;
+    $counters{uc $chr}->increment_range($pos, $pos + length($read) - 1);
+    say STDERR $counter if (++$counter % 10000 == 0);
+}
+
+for my $seq (sort $fasta_reader->sequence_list()) {
+    my $ba = $counters{uc $seq};
+    my $start = 1;
+    my $max = $fasta_reader->get_length($seq);
+    #say STDERR "$seq from $start to $max";
+    while ($start <= $max){
+        if ($window_size == 1){
+            my $value = $ba->{pdl}->at($start - $base); 
+            if ($value > 0 || $noskip){
+                say join "\t", $seq, qw/. ./, $start, $start, $value, qw/. . ./;
+            }
+            ++$start;
+        }
+        else{
+            my $end = $start + $window_size - 1;
+            if ($end > $max){
+                $end = $max;
+            }
+            my $pdl = $ba->get_range($start, $end);
+            my $count = $pdl->max();
+            if ($count > 0 || $noskip){
+                say join "\t", $seq, qw/. ./, $start, $end, $count, qw/. . ./;
+            }
+
+            $start += $window_size;
+        }
+    }
+}
+
+
+=head1 NAME
+
+bowtie_window.pl - return windows, with scores showing how many reads overlap that window
+
+=head1 SYNOPSIS
+
+Usage examples:
+
+ bowtie_window.pl [options]...
+
+=head1 OPTIONS
+
+=over
+
+=item --help | -h
+
+=back
+
+=cut
+
