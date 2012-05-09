a367e9bada6f79c5164d3510c353c90e80f08484
tnish <tnish@berkeley.edu>
2012-05-09 10:21:31 -0700

    Run::BowtieBuild force option

--- lib/Run/BowtieBuild.pm
+++ lib/Run/BowtieBuild.pm
@@ -17,7 +17,7 @@ our @ISA = qw(Exporter);
 our @EXPORT_OK = qw();
 our @EXPORT = qw(bowtie_build);
 
-=head2 bowtie_build(file => 'reference.fas', noref => BOOL, bs => ['c2t' | 'g2a'], rc => BOOL)
+=head2 bowtie_build(file => 'reference.fas', noref => BOOL, bs => ['c2t' | 'g2a'], rc => BOOL, force => BOOL)
 
 Returns (converted_file, index_prefix);
 
@@ -42,7 +42,11 @@ sub bowtie_build{
                 },
                 optional => 1,
             },
-            rc => 0,
+            rc => {
+                default => 1, 
+                optional => 1
+            },
+            force => 0,
         });
 
     if ($opt{bs} && $opt{rc}){
@@ -65,7 +69,7 @@ sub bowtie_build{
     my @expected_files = map { "$opt{index}.$_" } (qw/1.ebwt 2.ebwt rev.1.ebwt rev.2.ebwt/, $opt{noref} ? () : (qw/3.ebwt 4.ebwt/));
 
     my $norefarg = $opt{noref} ? '--noref' : '';
-    if (notall { -f && -s } @expected_files){
+    if ($opt{force} or notall { -f && -s } @expected_files){
         cast "bowtie-build $norefarg $opt{file} $opt{index}";
     }
     return ($opt{file}, $opt{index}, @expected_files);
