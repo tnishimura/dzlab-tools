b9a045c9a612bab740d9b054836517e9abbfe69e
tnish <tnish@berkeley.edu>
2012-08-17 15:17:16 -0700

    gff_stack_coords.pl --gap option

--- gff_stack_coords.pl
+++ gff_stack_coords.pl
@@ -14,41 +14,50 @@ use FindBin;
 use lib "$FindBin::Bin/lib";
 use GFF::Parser;
 
+use Pod::Usage;
+use Getopt::Long;
 
-my $last_seq; # last printed seq
-my $last_end; # last printed end
-my $offset = 0;
+my $result = GetOptions (
+    "gap|g=i" => \(my $gap = 5000),
+);
 
-if (! @ARGV && -t STDIN){
+if (! $result || (! @ARGV && -t STDIN)){
     pod2usage(-verbose => 2, -noperldoc => 1);
 }
 my $col1 = @ARGV == 1 ? basename($ARGV[0], '.gff') : 'stacked';
 
 my $p = GFF::Parser->new(file => \*ARGV);
+my $last;
+
+my $offset = 0;
 while (defined(my $gff = $p->next())){
     my ($seq, $orig_start, $orig_end, $score) = $gff->slice(1,4,5,6);
     $score //= '.';
 
+    # invalid line
     if (! defined($seq) || ! looks_like_number($orig_start) || ! looks_like_number($orig_end)){
         next;
     }
 
-    if (defined $last_seq && $last_seq ne $seq){
-        $offset = $last_end;
+    if (! $last){
+        $last = $gff;
+    }
+    elsif ( $last->sequence() ne $seq ){
+        $offset = $last->end() - $orig_start + $gap + 1;
     }
 
     my $start = $orig_start + $offset;
     my $end   = $orig_end + $offset;
 
     say join "\t", $col1, qw/. ./, $start, $end, $score, qw/. ./, "seq=$seq;start=$orig_start;end=$orig_end";
-    $last_seq = $seq;
-    $last_end = $end;
+    $last = $gff;
 }
 
 =head1 NAME
 
-gff_stack_coords.pl - Convert chromosome coordinates. written as one-shot for yvonne so she could feed 
-multi-chromosome GFF file to autocorrelation. Assumes inputs are sorted.
+gff_stack_coords.pl - Convert chromosome coordinates. written as one-shot for
+yvonne so she could feed multi-chromosome GFF file to autocorrelation. Assumes
+inputs are sorted.  Place --gap between chromosomes (default 5000);
 
 =head1 SYNOPSIS
 
@@ -57,6 +66,11 @@ Usage examples:
  gff_stack_coords.pl file.gff > output.gff
  gff_stack_coords.pl < file.gff > output.gff
  some_other_command.pl | gff_stack_coords.pl > output.gff
+
+With custom gap:
+
+ gff_stack_coords.pl --gap 10000 file.gff > output.gff
+ gff_stack_coords.pl -g 1000 < file.gff > output.gff
 
 =cut
 
