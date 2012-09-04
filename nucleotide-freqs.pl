13437075ba3067e47551c8e6dfe35693c8c999d4
tnish <tnish@berkeley.edu>
2012-09-04 13:29:50 -0700

    nucleotide-freqs.pl - dox

--- nucleotide-freqs.pl
+++ nucleotide-freqs.pl
@@ -1,53 +1,44 @@
 #!/usr/bin/env perl
-# ___UNDOCUMENTED___
-
 use warnings;
 use strict;
+use 5.010_000;
 use Data::Dumper;
-use Carp;
+use autodie;
+
 use Getopt::Long;
 use Pod::Usage;
+
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use FastaReader;
 
-
-# Check required command line parameters
-pod2usage ( -verbose => 1 )
-unless @ARGV;
-
-my $kmer = 2;
 my @excluded_sequences;
-my $alphabet = 'ACGT';
-my $output;
 
 # Grabs and parses command line options
 my $result = GetOptions (
-    'kmer|k=i'     => \$kmer,
+    'kmer|k=i'           => \(my $kmer = 2),
     'exclude-seq|x=s{,}' => \@excluded_sequences,
-    'alphabet|a=s' => \$alphabet,
-    'output|o=s' => \$output,
-    'verbose|v'  => sub { use diagnostics; },
-    'quiet|q'    => sub { no warnings; },
-    'help|h'     => sub { pod2usage ( -verbose => 1 ); },
-    'manual|m'   => sub { pod2usage ( -verbose => 2 ); }
+    'alphabet|a=s'       => \(my $alphabet = 'ACGT'),
+    'output|o=s'         => \(my $output),
 );
 
+pod2usage(-verbose => 2, -noperldoc => 1) if (!$result || ! @ARGV);
+
 if ($output) {
-    open my $USER_OUT, '>', $output or carp "Can't open $output for writing: $!";
+    open my $USER_OUT, '>', $output;
     select $USER_OUT;
 }
 
-my $reference = $ARGV[0];
+my $reference = shift;
 my %nucleotide_frequencies;
 
-print STDERR "# Reading in $reference";
+say STDERR "# Reading in $reference";
 my $fr = FastaReader->new(file => $reference, slurp => 0);
-print STDERR "# Done";
+say STDERR "# Done";
 
 for my $k (1, $kmer) {
     
-    print STDERR "#k:\t$k\n";
+    say STDERR "#k:\t$k";
 
     my %frequencies  = ();
     my $total_length = 0;
@@ -57,8 +48,8 @@ for my $k (1, $kmer) {
 
         $total_length += $chrlen;
         
-        print STDERR "#chromosome:\t$chromosome\n";
-        print STDERR "#length:\t", $chrlen, "\n";
+        say STDERR "#chromosome:\t$chromosome";
+        say STDERR "#length:\t", $chrlen;
 
         my %frequency = %{ word_composition ($fr->get($chromosome, undef, undef), $k) };
 
@@ -68,18 +59,18 @@ for my $k (1, $kmer) {
 
             $frequencies{$word} += $frequency{$word};
 
-            print STDERR join ("\t",
+            say STDERR join ("\t",
                                $word,
                                $frequency{$word},
                                $frequency{$word} / $chrlen, 
-                           ), "\n";
+                           );
         }
     }
 
-    print STDERR "#all:\n";
-    print STDERR "#length:$total_length\n";
+    say STDERR "#all:";
+    say STDERR "#length:$total_length";
 
-    print join ("\t",
+    say join ("\t",
                 '#word',
                 '#count',
                 '#size',
@@ -87,7 +78,7 @@ for my $k (1, $kmer) {
                 '#expected',
                 '#obs/expect',
                 '#independent',
-            ), "\n"
+            )
             if $k > 1;
 
     for my $word (sort keys %frequencies) {
@@ -96,17 +87,17 @@ for my $k (1, $kmer) {
 
         if ($k == 1) {
             $nucleotide_frequencies{$word} = $frequencies{$word} / $total_length;
-            print STDERR join ("\t",
+            say STDERR join ("\t",
                         $word,
                         $nucleotide_frequencies{$word},
-                    ), "\n";
+                    );
         }
         else {
             my $observed = $frequencies{$word} / $total_length;
             my $expected = 1;
             map {$expected *= $nucleotide_frequencies{$_}} (split //, $word);
 
-            print join ("\t",
+            say join ("\t",
                         $word,
                         $frequencies{$word},
                         $total_length,
@@ -114,12 +105,10 @@ for my $k (1, $kmer) {
                         $expected,
                         $observed / $expected,
                         map {$nucleotide_frequencies{$_} * $total_length} (split //, $word),
-                    ), "\n";
+                    );
         }
     }
 }
-
-
 
 sub word_composition {
     my ($sequence, $k, $alphabet) = @_;
@@ -137,59 +126,42 @@ sub word_composition {
     return \%frequency;
 }
 
-
-__END__
-
-
 =head1 NAME
 
- name.pl - Short description
+nucleotide-freqs.pl - calculate the nucleotide and nucleotide k-mer
+composition. 
 
 =head1 SYNOPSIS
 
-=head1 DESCRIPTION
+Calculate 1-mer and 2-mer composition in reference.fasta:
+
+ nucleotide-freqs.pl -o output.txt reference.fasta 
+ nucleotide-freqs.pl reference.fasta > output.txt
+
+Calculate 3-mers as well:
+
+ nucleotide-freqs.pl reference.fasta 
+
+ nucleotide-freqs.pl -o output.txt --kmer 3 reference.fasta 
+ nucleotide-freqs.pl -o output.txt -k     3 reference.fasta 
 
 =head1 OPTIONS
 
- name.pl [OPTION]... [FILE]...
+=over
 
- -o, --output      filename to write results to (defaults to STDOUT)
- -v, --verbose     output perl's diagnostic and warning messages
- -q, --quiet       supress perl's diagnostic and warning messages
- -h, --help        print this information
- -m, --manual      print the plain old documentation page
+=item  -k <count> | --kmer <count>
 
-=head1 REVISION
+Calculate composition up to this long. Default 2.  This means that A, C, G, T,
+AA, AC, AG, AT, ..., TG, TT composition are calculated.
 
- Version 0.0.1
+=item  -a <alpha> | --alphabet <alpha>
 
- $Rev: 249 $:
- $Author: psilva $:
- $Date: 2010-01-11 21:24:34 -0800 (Mon, 11 Jan 2010) $:
- $HeadURL: http://dzlab.pmb.berkeley.edu/svn/bisulfite/trunk/nucleotide-freqs.pl $:
- $Id: nucleotide-freqs.pl 249 2010-01-12 05:24:34Z psilva $:
+Alphabet of genome.  Default ACGT.  Use, say, ACGU for RNA.
 
-=head1 AUTHOR
+=item  -o <file> | --output <file>
 
- Pedro Silva <psilva@nature.berkeley.edu/>
- Zilberman Lab <http://dzlab.pmb.berkeley.edu/>
- Plant and Microbial Biology Department
- College of Natural Resources
- University of California, Berkeley
+Output file. Default to standard out.
 
-=head1 COPYRIGHT
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>.
+=back
 
 =cut
