69976a178abd0e26c819695f1cd0e88a09def91f
tnish <tnish@berkeley.edu>
2012-10-31 17:30:20 -0700

    FastqReader::Grep

---
+++ lib/FastqReader/Grep.pm
@@ -0,0 +1,37 @@
+package FastqReader::Grep;
+use strict;
+use warnings;
+use 5.010_000;
+use Data::Dumper;
+use Carp;
+use autodie;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw();
+our @EXPORT = qw(fastq_grep);
+
+sub fastq_grep{
+    my $pattern = shift;
+    croak "need a pattern" if ! $pattern;
+    my $infile = shift;
+    my $outfile = shift;
+
+    my $cmd = "grep --no-group-separator -B1 -A2";
+    say $cmd;
+
+    if ($infile && $outfile){
+        system("$cmd $pattern $infile > $outfile");
+    }
+    elsif ($infile && ! $outfile){
+        system("$cmd $pattern $infile");
+    }
+    elsif (! $infile && $outfile){
+        system("$cmd $pattern > $outfile");
+    }
+    else{
+        system("$cmd $pattern");
+    }
+}
+
+1;
