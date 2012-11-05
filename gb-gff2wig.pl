6935b782fe05422f42292dffb63a98a9ca7a5068
tnish <tnish@berkeley.edu>
2012-11-05 13:58:40 -0800

    gb-gff2wig.pl replaces gb-methylgff2wiggle.pl

---
+++ gb-gff2wig.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+use strict;
+use warnings FATAL => "all";
+use 5.010_000;
+use Data::Dumper;
+use autodie;
+use Pod::Usage;
+use Getopt::Long;
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+use GBUtil;
+
+my $result = GetOptions (
+    "file|f=s"   => \(my $file),
+    "dir|d=s"    => \(my $dir),
+    "compile|c"  => \(my $compile),
+    "bed|b"      => \(my $bed),
+    "ctscore|ct" => \(my $ctscore),
+);
+pod2usage(-verbose => 2, -noperldoc => 1) if (!$result || !$file);  
+
+say Dumper {
+    file    => $file,
+    dir     => $dir,
+    compile => $compile,
+    bed     => $bed,
+    ctscore => $ctscore,
+};
+
+gff_to_wig(
+    file    => $file,
+    dir     => $dir,
+    compile => $compile,
+    bed     => $bed,
+    ctscore => $ctscore,
+);
+
+=head1 gff2wig.pl 
+
+    file|f      (required)
+    dir|d       (optional, default '.')
+    compile|c   (optional, default no)
+    bed|b       (optional, default no)
+    ctscore|ct  (optional, default no)
+
+=cut
