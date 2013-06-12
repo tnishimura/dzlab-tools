a1093296811b51f68dc99311763faf6e224a2b48
T. Nishimura <tnish@fastmail.jp>
2013-06-12 15:13:57 -0700

    gff2tdf.pl - score-only option

--- gff2tdf.pl
+++ gff2tdf.pl
@@ -10,6 +10,7 @@ use List::MoreUtils qw/all/;
 use Parallel::ForkManager;
 use Pod::Usage;
 use Getopt::Long;
+use Scalar::Util qw/looks_like_number/;
 
 use FindBin;
 use lib "$FindBin::Bin/lib";
@@ -18,12 +19,12 @@ use FastaReader;
 use GFF::Statistics qw/gff_detect_width/;
 
 my $result = GetOptions (
-    "reference-genomee|r=s" => \(my $reference_genomee),
-    "igv-tools-jar|j=s" => \(my $igv_tools_jar = "$FindBin::Bin/share/igvtools.jar"),
-    # "score-from-ct|ct" => \(my $score_from_ct),
-    "tmp-dir|d=s" => \(my $tmp_dir),
+    "reference-genomee|r=s"      => \(my $reference_genomee),
+    "igv-tools-jar|j=s"          => \(my $igv_tools_jar = "$FindBin::Bin/share/igvtools.jar"),
+    "tmp-dir|d=s"                => \(my $tmp_dir),
     "no-feature-in-filename|nff" => \(my $no_feature_in_filename),
-    "parallel|p=i" => \(my $parallel=0),
+    "parallel|p=i"               => \(my $parallel=0),
+    "score-only|s"               => \(my $score_only),
 );
 pod2usage(-verbose => 2, -noperldoc => 1) 
 if (!$result || ! $igv_tools_jar || ! $reference_genomee || ! -f $igv_tools_jar || ! -f $reference_genomee);  
@@ -32,37 +33,29 @@ my $ref = FastaReader->new(file => $reference_genomee);
 my $pm = Parallel::ForkManager->new($parallel);
 
 # GFF high-density single-c/windows files
-for my $methylgff_file (@ARGV){
+for my $gff_file (@ARGV){
     $pm->start and next;
-    my $width = gff_detect_width($methylgff_file);
-    my $infh = IO::File->new($methylgff_file);
+    my $width = gff_detect_width($gff_file);
+    my $infh = IO::File->new($gff_file);
 
     # write each feature/seqid combo to seperate tmpfile, combine later
-    my %tmpfiles; # {methyl|coverage}{feature}{seqid}
+    my %tmpfiles; # {score|methyl|coverage}{feature}{seqid}
     while (defined(my $gffline = $infh->getline)){
         chomp $gffline;
-        my ($seq, undef, $feature, $start, $end, $score, undef, undef, $attr) = split /\t/, $gffline;
-	$seq = $ref->get_original_name($seq);
+        my @fields = split /\t/, $gffline;
+        next if @fields != 9;
 
-        use Scalar::Util qw/looks_like_number/;
-        # say $score if ! looks_like_number($score);
-        if (looks_like_number($score) and all { $_ ne '.' } $seq, $feature, $start, $end, $attr){
+        my ($seq, undef, $feature, $start, $end, $score, undef, undef, $attr) =  @fields;
+        $seq = $ref->get_original_name($seq);
+
+        # methylation is the default
+        if (! $score_only and all { $_ ne '.' } $seq, $feature, $start, $end, $attr){
             if (! exists $tmpfiles{methyl}{$feature}{$seq}){
                 $tmpfiles{methyl}{$feature}{$seq} = File::Temp->new(DIR => $tmp_dir, UNLINK => 1);
                 $tmpfiles{coverage}{$feature}{$seq} = File::Temp->new(DIR => $tmp_dir, UNLINK => 1);
                 my $mtmp = $tmpfiles{methyl}{$feature}{$seq}->filename;
                 my $ctmp = $tmpfiles{coverage}{$feature}{$seq}->filename;
                 warn "creating tmp files for $feature $seq ($mtmp, $ctmp)";
-                # variableStep chrom=chr1  span=50
-                # 0	0.083500
-                # 50	0.139000
-                # 100	0.119200
-                # 150	0.142900
-                # 200	0.035300
-                # 250	0.026800
-                # 300	0.041700
-                # $tmpfiles{methyl}{$feature}{$seq}->print("variableStep chrom=$seq  span=$width\n");
-                # $tmpfiles{coverage}{$feature}{$seq}->print("variableStep chrom=$seq  span=$width\n");
             }
             my ($c, $t);
             if ($attr=~/c=(\d+)/){ $c = $1; }
@@ -75,20 +68,34 @@ for my $methylgff_file (@ARGV){
                 $tmpfiles{coverage}{$feature}{$seq}->print("$start\t$coverage\n");
             }
         }
+
+        # --score-only is an addition to support non-methyl plain gff files with the score in col 6
+        # feature does not need to be non-'.' for score_only
+        if ($score_only and looks_like_number($score) and all { $_ ne '.' } $seq, $start, $end){
+            if (! exists $tmpfiles{score}{$feature}{$seq}){
+                $tmpfiles{score}{$feature}{$seq} = File::Temp->new(DIR => $tmp_dir, UNLINK => 1);
+                warn "creating tmp score file for $feature $seq";
+            }
+            $tmpfiles{score}{$feature}{$seq}->print("$start\t$score\n");
+        }
     }
     # combine each type/feature combination
-    for my $type (qw/methyl coverage/) {
+    for my $type (qw/score methyl coverage/) {
+        next if ! exists $tmpfiles{$type};
+
         my %feature2seq2fh = %{$tmpfiles{$type}};
         while (my ($feature,$seq2fh_ref) = each %feature2seq2fh) {
-		my $outfile = $methylgff_file;
-		if ($no_feature_in_filename){
-			$outfile =~ s/\.gff$/.$type.wig/; 
-		}
-		else{
-			$outfile =~ s/\.gff$/.$feature-$type.wig/;
-		}
-            my $outfh = IO::File->new($outfile, 'w');
+            my $wigfile = $gff_file;
+            if ($no_feature_in_filename || $feature eq '.'){
+                $wigfile =~ s/\.gff$/.$type.wig/; 
+            }
+            else{
+                $wigfile =~ s/\.gff$/.$feature-$type.wig/;
+            }
+            my $outfh = IO::File->new($wigfile, 'w');
 
+            # sort the tmp files, and append them to $wigfile
+            # yes, all sequences can be in the same file
             for my $seq (sort keys %$seq2fh_ref) {
                 my $tmpfh = $tmpfiles{$type}{$feature}{$seq};
                 my $tmpfile = $tmpfh->filename;
@@ -104,11 +111,13 @@ for my $methylgff_file (@ARGV){
                 while (defined(my $line = <$sortedfh>)){
                     $outfh->print($line);
                 }
+                close $sortedfh;
             }
             close $outfh;
-	    my $tdf = $outfile;
+
+            my $tdf = $wigfile;
             $tdf =~ s/\.wig$/\.tdf/;
-            system "java -jar $igv_tools_jar toTDF $outfile $tdf $reference_genomee";
+            system "java -jar $igv_tools_jar toTDF $wigfile $tdf $reference_genomee";
         }
     }
     $pm->finish; 
@@ -117,9 +126,41 @@ $pm->wait_all_children;
 
 =head1 gff2tdf.pl 
 
-Usage examples:
+This script converts a gff file into a TDF file suitable for loading into IGV.
+It requires that java is installed and available in your PATH.  It can handle
+methylation files (which is the default), which case it produces two tdf files
+(for methylation and coverage), or plain GFF files, in which case a single tdf
+file is created from the scores column (column 6).
 
- gff2tdf.pl [-p #threads] -r reference.fas input.gff
+This creates input.CG-methyl.tdf, input.CG-coverage.tdf, input.CG-methyl.tdf, etc..
+
+ gff2tdf.pl -r reference.fas input.gff
+
+This creates input.score.tdf:
+
+ gff2tdf.pl -s -r reference.fas input.gff
+
+In both cases, you can add a "-p #" argument to run in parallel.
+
+You should not redirect the output (don't use ">").
 
 =cut
 
+# wig files look like this:
+
+# variableStep chrom=chr1  span=50
+# 0	0.083500
+# 50	0.139000
+# 100	0.119200
+# 150	0.142900
+# 200	0.035300
+# 250	0.026800
+# 300	0.041700
+# variableStep chrom=chr2  span=50
+# 0	0.083500
+# 50	0.139000
+# 100	0.119200
+# 150	0.142900
+# 200	0.035300
+# 250	0.026800
+# 300	0.041700
