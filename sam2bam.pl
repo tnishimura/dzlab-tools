b0cff77aff6c9971d213e16dc890277106687579
T. Nishimura <tnish@fastmail.jp>
2013-07-01 19:42:45 -0700

    sam2bam.pl - remove Conjure dep

--- sam2bam.pl
+++ sam2bam.pl
@@ -5,8 +5,6 @@ use 5.010_000;
 use Data::Dumper;
 use autodie;
 use FindBin;
-use lib "$FindBin::Bin/lib";
-use Conjure;
 use Pod::Usage;
 use Getopt::Long;
 
@@ -14,7 +12,9 @@ my $result = GetOptions (
     "n|no-index" => \(my $no_index),
 );
 
-if (@ARGV == 0){ say STDERR  "$0 input.sam"; exit 1}
+use File::Basename qw/basename/;
+$0 = basename($0);
+if (@ARGV == 0){ say STDERR "$0 input.sam"; exit 1}
 
 for my $sam (@ARGV) {
     # my $bam_sorted_prefix = $sam =~ s/\.sam$//r;
@@ -24,16 +24,10 @@ for my $sam (@ARGV) {
     my $bam_sorted        = "$bam_sorted_prefix.bam";
     my $bam_index         = "$bam_sorted_prefix.bam.bai";
 
-    conjure(program => qq{ samtools view -bS $sam > $bam_unsorted },
-        on_stderr => sub { say },
-    );
-    conjure(program => qq{ samtools sort $bam_unsorted $bam_sorted_prefix },
-        on_stderr => sub { say },
-    );
-    conjure(program => qq{ samtools index $bam_sorted },
-        on_stderr => sub { say },
-    ) unless $no_index;
-    # conjure(program => qq{ samtools tview $bam_sorted });
+    system(qq{ samtools view -bS $sam > $bam_unsorted });
+    system(qq{ samtools sort $bam_unsorted $bam_sorted_prefix });
+    system(qq{ samtools index $bam_sorted });
+    # qq{ samtools tview $bam_sorted }
 
     unlink $bam_unsorted;
 }
