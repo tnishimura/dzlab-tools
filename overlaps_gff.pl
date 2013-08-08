782374ab43afa3f29aae735357073aede2d2a9a1
T. Nishimura <tnish@fastmail.jp>
2013-08-07 17:44:08 -0700

    overlaps_gff.pl -- added upstream/downstream/no-body searching

--- overlaps_gff.pl
+++ overlaps_gff.pl
@@ -10,10 +10,21 @@ use lib "$FindBin::Bin/Tree-Range/lib";
 use GFF::Tree;
 use Getopt::Euclid qw( :vars<opt_> );
 use Pod::Usage;
-
+use Scalar::Util qw/refaddr/;
 
 pod2usage(-verbose => 99,-sections => [qw/NAME SYNOPSIS OPTIONS/]) 
 if $opt_help || !$opt_gff || !$opt_input;
+
+if ($opt_no_body && ! $opt_downstream && ! $opt_upstream){
+    say STDERR "You specified -nb/--no-body, but not -down or -up. You have to look for something." ;
+    exit 1;
+}
+if ($opt_proportion_threshold != 0 && ($opt_no_body || $opt_downstream || $opt_upstream)){
+    say STDERR "Sorry, -p / --proportion-threshold is not currently compatible with --no-body, -down or -up." ;
+    exit 1;
+}
+
+
 
 if ($opt_output ne '-'){
     open my $fh, '>', $opt_output;
@@ -39,7 +50,19 @@ while (defined(my $gff = $p->next())){
     my $outputted;
 
     if ($annotation_has_seq{uc $seq}){
-        my @results = $gt->search_overlap($seq,$start,$end);
+        # shove them in a hash first, with address as keys so values are unique
+        my %results_set;
+        if (! $opt_no_body){
+            $results_set{refaddr $_} = $_ for $gt->search_overlap($seq,$start,$end);
+        }
+        if ($opt_downstream > 0){
+            $results_set{refaddr $_} = $_ for $gt->search_overlap($seq,$end + 1,$end + $opt_downstream);
+        }
+        if ($opt_upstream > 0){
+            $results_set{refaddr $_} = $_ for $gt->search_overlap($seq,$start -  $opt_upstream, $start - 1);
+        }
+
+        my @results = values %results_set;
 
         for my $result (@results){
             my $overlap = $result->{overlap};
@@ -47,6 +70,7 @@ while (defined(my $gff = $p->next())){
                 ($opt_proportion_threshold == 1 && $overlap == $gff->length) || 
                 $overlap / $gff->length() >= $opt_proportion_threshold
             ){
+
                 if (defined $opt_tag && defined(my $locus = $result->{item}->get_column($opt_tag))){
                     if ($opt_tag_only){
                         $gff->attribute_string($locus);
@@ -133,6 +157,35 @@ reported with a null score). Default to 0, mean any overlap is enough.
 
 Only print one entry for multiply overlapping input.
 
+=item  -up <bp> | --upstream <bp>
+
+Search <bp> upstream of input windows.
+
+=for Euclid
+    bp.default:     0
+    bp.type:        int, bp >= 0 
+    bp.type.error:  <bp> must be greater than 0
+
+=item  -down <bp> | --downstream <bp>
+
+Search <bp> downstream of input windows.
+
+=for Euclid
+    bp.default:     0
+    bp.type:        int, bp >= 0 
+    bp.type.error:  <bp> must be greater than 0
+
+=item --no-body | -nb
+
+Do not look for overlaps with body only.  This is useful when looking for overlaps with 
+upstream/downstream regions only.  For example, -nb -down 100 -up 100 searches for overlaps
+with 100 bp upstream and downstream of a input window, but NOT the window itself.
+
+=item --col8-marker | -m
+
+Replace column 8 (normally the GFF 'frame' column) with 1 when input window has
+an overlap.  This is useful when used with --no-skip, so that you can quickly
+differentiation between windows with or without overlaps.
 
 =item --help | -h
 
@@ -142,22 +195,3 @@ Only print one entry for multiply overlapping input.
 
 
 
-# =item  -up <bp> | --upstream <bp>
-# 
-# =for Euclid
-#     bp.default:     0
-#     bp.type:        int, bp >= 0 
-#     bp.type.error:  <bp> must be greater than 0
-# 
-# =item  -down <bp> | --downstream <bp>
-# 
-# =for Euclid
-#     bp.default:     0
-#     bp.type:        int, bp >= 0 
-#     bp.type.error:  <bp> must be greater than 0
-# 
-# =item --no-body | -nb
-# 
-# Do not look for overlaps with body only.  This is useful when looking for overlaps with 
-# upstream/downstream regions only.  For example, -nb -down 100 -up 100 searches for overlaps
-# with 100 bp upstream and downstream of a input window, but NOT the window itself.
