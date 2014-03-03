0d79bd3d25ad06f965eceb7b735fa7c886b5abdc
T. Nishimura <tnish@fastmail.jp>
2014-03-03 14:21:15 -0800

    pad-scaffold.pl make labeling strand specific. add minimum size for pad

--- pad-scaffold.pl
+++ pad-scaffold.pl
@@ -5,18 +5,18 @@ use autodie;
 use Data::Dumper;
 use FindBin;
 use Pod::Usage;
-use List::Util qw/first max min shuffle sum/;
+use List::Util qw/max min sum/;
 use Getopt::Long;
 use lib "$FindBin::Bin/lib";
 use DZUtil qw/overlap/;
 use GFF::Parser;
 use GFF::Tree;
 
-
 my $result = GetOptions (
     "debug|d"      => \(my $debug),
     "pad-size|s=i" => \(my $pad_size = 300),
     "interpad|p=i" => \(my $interpad = 0),
+    "minimum-pad-size|m=i"  => \(my $minimum_pad_size = 50),
     "attribute-id|i=i" => \(my $id_tag = "Parent"),
 );
 my $gff_name = shift;
@@ -46,12 +46,20 @@ for my $id (sort keys %id_to_isoforms) {
     die "no isoforms?" if @isoforms == 0;
 
     my $seqid = $isoforms[0]->sequence;
+    my $strand         = $isoforms[0]->strand;
+    my $is_reverse     = $strand eq '-';
+    # in the code, upstream means closer to position 1 
+    # in the gff, upstream means upstream from the 5' end of the isoform
+    my $upstream_pad   = $is_reverse ? 'downstream_pad' : 'upstream_pad'; 
+    my $downstream_pad = $is_reverse ? 'upstream_pad' : 'downstream_pad';
+    my $upstream_attribute = "ID=$id:$upstream_pad;Parent=$id";
+    my $downstream_attribute = "ID=$id:$downstream_pad;Parent=$id";
     my $isoform_start = $isoforms[0]->start;
     my $isoform_end = $isoforms[$#isoforms]->end;
     my $chromosome_start = 1;
     my $chromosome_end = 999_999_999_999; # fix me : get chromosome size from fasta and bound upstream
 
-    # check that 
+    # check that there is space upstream
     if ($isoform_start != 1){
         #my $proposed_upstream_start = max($isoform_start - $pad_size, $chromosome_start) ;
         #my $proposed_upstream_end   = max($isoform_start - 1, $chromosome_start) ;
@@ -86,7 +94,7 @@ for my $id (sort keys %id_to_isoforms) {
         }
         if ($accepted){
             die "why is upstream flipped?" if $proposed_upstream_end < $proposed_upstream_start;
-            push @padding, [ $seqid, $isoforms[0]->source, "upstream_pad", $proposed_upstream_start, $proposed_upstream_end, ".", ".", ".", "$id_tag=$id", ];
+            push @padding, [ $seqid, $isoforms[0]->source, $upstream_pad, $proposed_upstream_start, $proposed_upstream_end, ".", $strand, ".", $upstream_attribute ];
         }
     }
     for my $iso (@isoforms) {
@@ -124,17 +132,18 @@ for my $id (sort keys %id_to_isoforms) {
         }
         if ($accepted){
             die "why is downstream flipped?" if $proposed_downstream_end < $proposed_downstream_start;
-            push @padding, [ $seqid, $isoforms[0]->source, "downstream_pad", $proposed_downstream_start, $proposed_downstream_end, ".", ".", ".", "$id_tag=$id",];
+            push @padding, [ $seqid, $isoforms[0]->source, $downstream_pad, $proposed_downstream_start, $proposed_downstream_end, ".", $strand, ".", $downstream_attribute,];
         }
     }
 }
 
-#if ($debug){ say "$_ " . length($id_to_isoforms{$_}) for keys %id_to_isoforms; }
+# get rid of tiny pads
+@padding = grep { abs($_->[4] - $_->[3]) > 5 } @padding;
 
-for my $i (0 .. $#padding - 1) {
+for (my $i = 0 ; $i < $#padding ; $i++){
     my $this = $padding[$i];
     my $next = $padding[$i + 1];
-    next unless ($this->[2] eq 'downstream_pad' and $next->[2] eq 'upstream_pad');
+    # next unless ($this->[2] eq 'downstream_pad' and $next->[2] eq 'upstream_pad');
     my $this_start = $this->[3];
     my $this_end   = $this->[4];
     my $next_start = $next->[3];
@@ -147,21 +156,25 @@ for my $i (0 .. $#padding - 1) {
         ){
             die "why are there engulfings? " . join(",",@$this) . "\n" . join(",", @$next);
         }
+
         my $middle = int(($this_end + $next_start) / 2);
         $this->[4] = $middle - int($interpad / 2);
         $next->[3] = $middle + int($interpad / 2) + 1;
+        $i++; # skip next
     }
 }
 
 for my $pad (@padding) {
-    say join "\t", @$pad;
+    if ($pad->[4] - $pad->[3] + 1> $minimum_pad_size){
+        say join "\t", @$pad;
+    }
 }
 
 =head1 pad_scaffold.pl 
 
 Add padding to exon annotation file.
 
- pad_scaffold.pl [--pad-size 300 | -s 300] single-isoform-annotation.gff > with-padding.gff
+ pad_scaffold.pl [--minimum-pad-size 50 | -m 50] [--pad-size 300 | -s 300] single-isoform-annotation.gff > with-padding.gff
 
 This was written for Jessica, originally for single-isoform rice annotation files.
 
