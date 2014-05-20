51bb6b531049ee87f7f2bd5274195428de66d5f6
T. Nishimura <tnish@fastmail.jp>
2014-05-20 00:46:50 -0700

    install-deps.bat

--- util/install-deps.bat
+++ util/install-deps.bat
@@ -1,1 +1,4 @@
+cpan App::cpanminus
 cpanm Config::General DBI Devel::Size File::Which Getopt::Euclid IO::All IPC::Run List::MoreUtils Log::Dispatch Log::Log4perl Math::BigInt Math::BigFloat Moose MooseX::Singleton Mouse Parallel::ForkManager Params::Validate Path::Class PDL PDL::Stats Perl6::Slurp POE Readonly Smart::Comments Statistics::Descriptive Term::ProgressBar Test::Exception Tie::IxHash URI XML::Simple YAML Text::CSV_XS DBD::SQLite LWP::Simple Test::Deep::NoTest Getopt::Std IO::Scalar IPC::Cmd
+cpanm http://search.cpan.org/CPAN/authors/id/F/FA/FANGLY/Getopt-Euclid-0.3.6.tar.gz
+
