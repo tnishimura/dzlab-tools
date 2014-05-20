602bcf588b292e193f2adea43cc0d69481e480b1
T. Nishimura <tnish@fastmail.jp>
2014-05-20 00:08:01 -0700

    apt-get deps

---
+++ util/apt-get-deps.sh
@@ -0,0 +1,8 @@
+sudo apt-get cpanminus libconfig-general-perl libdbi-perl libdevel-size-perl libfile-which-perl libio-all-perl libipc-run-perl liblist-moreutils-perl liblog-dispatch-perl liblog-log4perl-perl libmath-bigint-perl libmath-bigint-perl libmoose-perl libmoosex-singleton-perl libmouse-perl libparallel-forkmanager-perl libparams-validate-perl libpath-class-perl pdl libpdl-stats-perl libperl6-slurp-perl libpoe-perl libreadonly-perl libsmart-comments-perl libstatistics-descriptive-perl libterm-progressbar-perl libtest-exception-perl libtie-ixhash-perl liburi-perl libxml-simple-perl libyaml-perl libtext-csv-xs-perl libdbd-sqlite3-perl libwww-perl 
+sudo http://search.cpan.org/CPAN/authors/id/F/FA/FANGLY/Getopt-Euclid-0.3.6.tar.gz
+
+# not in deb
+# Test::Deep::NoTest
+# Getopt::Std
+# IO::Scalar
+# IPC::Cmd
